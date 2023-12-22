dae4cc745eda16c9b379d4b9572c1d90b0753d83
Angel <dreben75@icloud.com>
2023-12-22 13:02:44 +0200

    fix

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add grades page', async ({ page }) => {
-    await page.goto('http://localhost:8090/Add-Grade');
+    await page.goto('http://localhost:8090/Add-Grades');
     const form = await page.$('textfield');
     expect(form).toBeTruthy();
   });
